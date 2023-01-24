35a50fd47a40f3e5d963690e8f310f4a7fa50f15
Javier Prieto Ortiz <jpojaviprior@gmail.com>
2023-01-24 09:52:02 +0100

    Calculator finished

--- IncredibleCalculator.java
+++ IncredibleCalculator.java
@@ -16,14 +16,39 @@ public class IncredibleCalculator {
 
         switch (opcion) {
             case 1: //Sumar. Debes leer dos numeros enteros de entrada e imprimir su suma
-                    break;
+                System.out.println("Indique el primer número a sumar");
+                int n1 = leerEnteroEntrada();
+                System.out.println("Indique el segundo número a sumar");
+                int n2 = leerEnteroEntrada();
+                System.out.println("La suma tiene como resultado " + suma(n1, n2));
+                break;
             case 2: // Restar. Debes leer dos números enteros de entrada e imprimir su resta
-                    break;
+                System.out.println("Indique el primer número a restar");
+                n1 = leerEnteroEntrada();
+                System.out.println("Indique el segundo número a restar");
+                n2 = leerEnteroEntrada();
+                System.out.println("La resta tiene como resultado " + resta(n1, n2));
+                break;
             case 3: // Multiplicar. Debes leer dos números enteros de entrada e imprimir su producto.
+                System.out.println("Indique el primer número a multiplicar");
+                n1 = leerEnteroEntrada();
+                System.out.println("Indique el segundo número a multipllicar");
+                n2 = leerEnteroEntrada();
+                System.out.println("La resta tiene como resultado " + multiplicacion(n1, n2));
                     break;
             case 4: // Multiplicar. Debes leer dos números enteros de entrada e imprimir su división entera.
+                System.out.println("Indique el dividendo");
+                n1 = leerEnteroEntrada();
+                System.out.println("Indique el divisor");
+                n2 = leerEnteroEntrada();
+                System.out.printf("La división tiene como resultado %.2f", divisionEntera(n1, n2));
                     break;
             case 5: // Multiplicar. Debes leer dos números enteros de entrada e imprimir su división con decimales.
+                System.out.println("Indique el dividendo");
+                n1 = leerEnteroEntrada();
+                System.out.println("Indique el divisor");
+                n2 = leerEnteroEntrada();
+                System.out.printf("La división tiene como resultado %.2f", divisionConDecimales(n1, n2));
                     break;
 
         }
